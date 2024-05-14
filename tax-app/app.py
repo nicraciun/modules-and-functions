21a7a81fe4a3662c013d5c59fa8f5a6ad3ea4fbd
nicraciun <nick.craciun@gmail.com>
2024-05-14 20:06:01 +0300

    Modifed

--- tax-app/app.py
+++ tax-app/app.py
@@ -1,8 +1,18 @@
 from tax import *
+import re
+
 while True:
     print ("-"*40) 
-    amount = int(input("Amount >> "))
-    goal = int(input("Goal (Salary = 1, loan = 2, Donation = 3, Exit = any key) >> "))
+    amount = input("Amount >> ")
+    goal = input("Goal (Salary = 1, loan = 2, Donation = 3, Exit = any key) >> ")
+
+    if re.match("^[0-9]+$", amount) and re.match("^[0-9]+$", goal):
+        amount = int(amount)
+        goal = int(goal) 
+    else:
+        print("You did not enter a number")
+        continue
+
     print ("#"*40) 
     if goal == 1 or goal == 2 or goal == 3:
         tax = calculateTax(amount, goal)
@@ -18,3 +28,4 @@ while True:
         print ("#"*40)
     else: 
         break
+   
